e775c69606642cd325adeb69406a5a49a86112f9
Victor <deyanovitch77580@gmail.com>
2020-07-12 23:57:07 +0200

    Fix logo

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,6 +1,5 @@
 import 'package:esgi_project/utils/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 class Logo extends StatelessWidget {
@@ -9,11 +8,10 @@ class Logo extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Lottie.asset(Constant.lottieLogo, width: 250, height: 300),
-        Text(
-          "WE\nMOUV",
-          style: Get.textTheme.headline1,
-        )
+        Lottie.asset(Constant.lottieLogo, width: 150, height: 300),
+        Text("WE\nMOUV",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30))
       ],
     );
   }
